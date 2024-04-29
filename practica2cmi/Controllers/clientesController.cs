a3973fca9712c727da27eb977618a631cf0484ea
Jairo Jaenz <jairojaenz91@gmail.com>
2024-04-29 11:18:38 -0600

    Dashboard completo

---
+++ practica2cmi/Controllers/clientesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practica2cmi.Models;
+
+namespace Practica2cmi.Controllers
+{
+    public class clientesController : Controller
+    {
+        private BDpruebaEntities db = new BDpruebaEntities();
+
+        // GET: clientes
+        public ActionResult Index()
+        {
+            var clientes = db.clientes.Include(c => c.usuario);
+            return View(clientes.ToList());
+        }
+
+        // GET: clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = db.clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: clientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nombreUsuario");
+            return View();
+        }
+
+        // POST: clientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,apellido,idUsuario")] cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.clientes.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nombreUsuario", cliente.idUsuario);
+            return View(cliente);
+        }
+
+        // GET: clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = db.clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nombreUsuario", cliente.idUsuario);
+            return View(cliente);
+        }
+
+        // POST: clientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,apellido,idUsuario")] cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idUsuario = new SelectList(db.usuarios, "idUsuario", "nombreUsuario", cliente.idUsuario);
+            return View(cliente);
+        }
+
+        // GET: clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = db.clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cliente cliente = db.clientes.Find(id);
+            db.clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- practica2cmi/Models/prueba.Designer.cs
+++ practica2cmi/Models/prueba.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\user\source\repos\Practica2cmi\Practica2cmi\Models\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Practica2cmi\Practica2cmi\Models\prueba.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
